03d311a0b25e871524660cf639c78fba17e77d82
horiaradwan@gmail.com <horiaradwan22@gmail.com>
2024-03-09 22:32:03 +0200

    1.8: unicafe step3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
+const Header=({text})=><h1>{text}</h1>
 const Button=({onSmash,text})=><button onClick={onSmash}>{text}</button>
+const Statistics = (props) =><Button onSmash={props.onSmash} text={props.text}/>
+const Values=(props)=>{return <div>{props.text} {props.value}</div>}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const total=good+neutral+bad
   const avg=(good+neutral+bad)/3
-  const positisve=()=>{
-     if (total===0) {
-       return 0
-     }return(good /total)*100}
-  
-  return (
+  const positive=()=>{
+         if (total===0) {
+             return " "
+          }return(good /total)*100+"%"}
+return (
     <div>
-      <h1>Give Feedback</h1>
-      <div><Button onSmash={()=>(setGood(good+1))} text="good"/>
-      <Button onSmash={()=>(setNeutral(neutral+1))} text="neutral"/>
-      <Button onSmash={()=>(setBad(bad+1))} text="bad"/>
-      </div>
-      <h1>Statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>total {total}</div>
-      <div>average {avg}</div>
-      <div>positive {positisve()}%</div>
-   </div>
+      <Header text="Give Feedback"/>
+       <Statistics onSmash={()=>(setGood(good+1))} text="good"/>
+       <Statistics onSmash={()=>(setNeutral(neutral+1))} text="neutral"/>
+       <Statistics onSmash={()=>(setBad(bad+1))} text="bad"/>
+       <Header text="Statistics"/>
+       <Values text="good" value={good}/>
+       <Values text="neutral" value={neutral}/>
+       <Values text="bad" value={bad}/>
+       <Values text="all" value={total}/>
+       <Values text="average" value={avg}/>
+       <Values text="positive" value={positive()}/>
+</div>
   )
 }
-
-
-export default App;
+ export default App;
