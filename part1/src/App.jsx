b505283e822928489f59c5e9947cbdea4c4b5c8c
horiaradwan@gmail.com <horiaradwan22@gmail.com>
2024-03-09 21:49:56 +0200

    1.1 step1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,89 +1,32 @@
-import { useState ,useEffect} from 'react'
-import ContentService from './services/ContentService'
-import Person from './Components/Person'
-import Filter from './Components/Filter'
-const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newphoneeumber, setNewphone] = useState('')
-  const [searchQuery,setsearchQuery]=useState('')
-  const [previousId,setPreviousId]=useState(null)
-
-   useEffect(()=>{
-    ContentService.getAll()
-    .then(response=>{
-      setPersons(response.data)
-    })
-
-  },[])
-
-  useEffect(() => {
-    if (persons.length > 0) {
-    setPreviousId(parseInt(persons[persons.length - 1].id));
-    }
-  }, [persons]);
 
 
-const AddPersonData = (event) => {
-  event.preventDefault()
-  if (persons.some(x => x.name === newName && x.phonenumber===newphoneeumber)) {
-    window.alert(newName+" with "+ newphoneeumber+' is already added to phonebook')
-    
-  }
-  else {
-    const newObject = {
-      id: (previousId ? previousId + 1 : 1).toString(),
-      name: newName,
-      phonenumber:newphoneeumber ,
-      
-    }
-     ContentService.create(newObject)
-      .then(response=>{
-        console.log(response.data)
-        setPersons(NewPersons => [...NewPersons, newObject])
-        setNewName("") 
-        setNewphone("") 
-      })}
-  
+const Header =(props)=>{
+  return <div><h1>{props.course}</h1></div>
 }
-const filter=persons.filter(x=>x.name.toLowerCase()
-.includes(searchQuery.toLowerCase()))
-
-
-const toDelete = (id,name) => {
-  if (window.confirm("Delete "+ name+" ?")){
-    ContentService.todelete(id)
-      .then(() => {
-      setPersons(persons.filter(person => person.id !== id))})
-      }
-    }
-
-return (
-  <div>
-  <h2>Phonebook</h2>
-  <Filter search={searchQuery} setsearch={()=>setsearchQuery(event.target.value)}/>
- 
-  <h2>add a new</h2>
-
-  <form onSubmit={AddPersonData}>
-    <div> Name: <input value={newName}
-      onChange={event=>setNewName(event.target.value)}/></div>
-      <div> Number: <input value={newphoneeumber}
-      onChange={event=>setNewphone(event.target.value)}/></div>
-    <div>
-      <button type="submit">add</button>
-    </div>
-  </form>
-  <h2>Numbers</h2>
-  {filter.map(x=>
-    <Person key={x.id} person={x}
-    Delete={()=>toDelete(x.id,x.name)}/>)}
-
-
+const Content =(C)=>{
+  return <div><p>{C.part1} {C.exercises1}</p>
+  <p>{C.part2} {C.exercises2}</p>
+  <p>{C.part3} {C.exercises3}</p>
   </div>
-  )
 }
-   
-
-export default App
-
+const Total=(t)=>{
+  return <div> <p>Number of exercises {t.total} </p></div>
+}
+const App =()=>{
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const total=exercises1+exercises2+exercises3
+  return <div>
+   <Header course={course}/>
+   <Content part1={part1} exercises1={exercises1}  
+   part2={part2} exercises2={exercises2}
+   part3={part3} exercises3={exercises3}/>
+   <Total total={total}/>
+  </div>
+}
+export default App;
