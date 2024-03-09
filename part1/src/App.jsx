c0323723302155df0f48c7a53e628240ef47e60e
horiaradwan@gmail.com <horiaradwan22@gmail.com>
2024-03-09 22:38:41 +0200

    1.12*: anecdotes step1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,57 +1,29 @@
 import { useState } from 'react'
-const Header=({text})=><h1>{text}</h1>
-const Button=({onSmash,text})=><button onClick={onSmash}>{text}</button>
-const Statistics = (props) =><Button onSmash={props.onSmash} text={props.text}/>
-const StatisticLine=(props)=>{return <div>
- <table >
-   <tbody>
-     <tr>
-       <td>{props.text}</td>
-       <td >{props.value}</td>
-     </tr>
-   </tbody>
- </table>
-</div>}
-const Condition=({good,neutral,bad,total,avg,positive})=>{
- if(good===0&&neutral===0&&bad ===0){
-   return "No Given feedback"
- }return(
-   <div>
-   <StatisticLine text="good" value={good}/>
-   <StatisticLine text="neutral" value={neutral}/>
-   <StatisticLine text="bad" value={bad}/>
-   <StatisticLine text="all" value={total}/>
-   <StatisticLine text="average" value={avg}/>
-   <StatisticLine text="positive" value={positive}/>
-   </div>
- )
-
-}
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const total=good+neutral+bad
-  const avg=(good-bad)/total
-  const positive=()=>(good /total)*100+"%"
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const handelanecdotes=()=>{
+    let number=Math.floor(Math.random()*8)
+    console.log(number)
+    setSelected(number)
+   } 
 return (
     <div>
-      <Header text="Give Feedback"/>
-       <Statistics onSmash={()=>(setGood(good+1))} text="good"/>
-       <Statistics onSmash={()=>(setNeutral(neutral+1))} text="neutral"/>
-       <Statistics onSmash={()=>(setBad(bad+1))} text="bad"/>
-       <Header text="Statistics"/>
-       <Condition good={good} neutral={neutral} bad={bad}
-       total={total} avg={avg} positive={positive()}/>
-
-
-
-
-
-
+      {anecdotes[selected]}
+     <div><button onClick={handelanecdotes}>next anecdote</button></div> 
      
-</div>
+    </div>
   )
 }
- export default App;
+export default App
