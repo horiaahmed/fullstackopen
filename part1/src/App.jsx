dba2098eeee06b1877406d6eccfbd99618a6adfd
horiaradwan@gmail.com <horiaradwan22@gmail.com>
2024-03-09 22:29:59 +0200

    1.7: unicafe step2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-const Button=({onSmash,text})=>{
- return <button onClick={onSmash}>{text}</button>
-
-}
-
+const Button=({onSmash,text})=><button onClick={onSmash}>{text}</button>
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
- 
+  const total=good+neutral+bad
+  const avg=(good+neutral+bad)/3
+  const positisve=()=>{
+     if (total===0) {
+       return 0
+     }return(good /total)*100}
+  
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -20,8 +22,10 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
-
-    </div>
+      <div>total {total}</div>
+      <div>average {avg}</div>
+      <div>positive {positisve()}%</div>
+   </div>
   )
 }
 
