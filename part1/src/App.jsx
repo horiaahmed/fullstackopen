3fb8faab46157b07db0c76d9840db6f3e6b2c297
horiaradwan@gmail.com <horiaradwan22@gmail.com>
2024-03-09 22:19:04 +0200

    1.5 step6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,49 +1,30 @@
-//1.5
-const Header =(props)=>{
-  return <div><h1>{props.course.name}</h1></div>
-}
-const Part=(props)=>{
-  return <div>
-  <p>
-    {props.parts.name} {props.parts.exercises}
-    </p> 
-  </div>
+import { useState } from 'react'
+const Button=({onSmash,text})=>{
+ return <button onClick={onSmash}>{text}</button>
+
 }
 
-const Content =(props)=>{
-  return <div>
-    <Part parts={props.content.parts[0]}/>
-    <Part parts={props.content.parts[1]}/>
-    <Part parts={props.content.parts[2]}/>
-
-     </div>
-}
-const Total=(props)=>{
-  return <div> <p>Number of exercises {props.total} </p></div>
-}
 const App = () => {
-    const course = {
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7
-        },
-        {
-          name: 'State of a component',
-          exercises: 14
-        }
-      ]
-    }
-  const total=course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises
-  return <div>
-   <Header course={course}/>
-   <Content content={course} />
-   <Total total={total}/>
-  </div>
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+ 
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <div><Button onSmash={()=>(setGood(good+1))} text="good"/>
+      <Button onSmash={()=>(setNeutral(neutral+1))} text="neutral"/>
+      <Button onSmash={()=>(setBad(bad+1))} text="bad"/>
+      </div>
+      <h1>Statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+
+    </div>
+  )
 }
+
+
 export default App;
